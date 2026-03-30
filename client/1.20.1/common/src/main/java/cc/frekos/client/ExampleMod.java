48d2a07594c2705f3d8f6654cc3d58e5d0d32551
Frekvens1 <Frekvens1@gmail.com>
2026-03-30 19:24:36 +0200

    #46 - Installed Architectury

---
+++ client/1.20.1/common/src/main/java/cc/frekos/client/ExampleMod.java
@@ -0,0 +1,9 @@
+package cc.frekos.client;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "frekos_client";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
