2cf9bb2a58942e67adeeb50c7df9964b7ee68b91
Frekvens1 <Frekvens1@gmail.com>
2026-04-04 03:18:52 +0200

    #65 - Added tps app

--- backend/frekos-install.lua
+++ backend/frekos-install.lua
@@ -16,6 +16,7 @@ local downloads = {
     -- region { System files - Applications }
 
     "/frekos/apps/cat.lua",
+    "/frekos/apps/tps.lua",
     "/frekos/apps/update.lua",
     "/frekos/apps/lockscreen.lua",
     "/frekos/apps/clean_install.lua",
